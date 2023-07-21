397b1de3024bcbd0288352a719ca3a7720d26da0
ward <2501362421@qq.com>
2023-07-22 01:06:31 +0800

    Fix: add Http2 & Https support

--- cmd/api/initialize/tls.go
+++ cmd/api/initialize/tls.go
@@ -7,13 +7,14 @@ import (
 )
 
 // InitTLS
+// 自签名证书生成：openssl req -x509 -newkey rsa:4096 -keyout server.key -out server.crt -days 365 -nodes
 func InitTLS() *tls.Config {
 	cfg := &tls.Config{
 		MinVersion:         tls.VersionTLS10,
 		InsecureSkipVerify: true,
 	}
-	cert, err := tls.LoadX509KeyPair("cmd/api/cert/server.crt",
-		"cmd/api/cert/server.key")
+	cert, err := tls.LoadX509KeyPair("./cert/server.crt",
+		"./cert/server.key")
 	if err != nil {
 		hlog.Fatal("tls failed", err)
 	}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,17 +6,19 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	// "time"
+	"time"
 
 	"tiktok-demo/cmd/api/config"
 	"tiktok-demo/cmd/api/initialize"
 	"tiktok-demo/cmd/api/initialize/rpc"
+	"tiktok-demo/shared/consts"
 
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/app/server"
+
 	// "github.com/hertz-contrib/cors"
-	// cfg "github.com/hertz-contrib/http2/config"
-	// "github.com/hertz-contrib/http2/factory"
+	cfg "github.com/hertz-contrib/http2/config"
+	"github.com/hertz-contrib/http2/factory"
 	hertztracing "github.com/hertz-contrib/obs-opentelemetry/tracing"
 	hertzSentinel "github.com/hertz-contrib/opensergo/sentinel/adapter"
 	"github.com/hertz-contrib/pprof"
@@ -36,23 +38,26 @@ func main() {
 	// create a new server
 	h := server.New(
 		tracer,
-		server.WithALPN(true),
+		// server.WithALPN(true),
 		// server.WithTLS(tlsCfg),
 		server.WithHostPorts(fmt.Sprintf(":%d", config.GlobalServerConfig.Port)),
 		server.WithRegistry(r, info),
 		server.WithHandleMethodNotAllowed(true),
-		server.WithMaxRequestBodySize(1024*1024*1024),
+		server.WithMaxRequestBodySize(1024*1024*1024), // 1G上传限制
 	)
 	// add http2 protocol
-	// h.AddProtocol("h2", factory.NewServerFactory(
-	// 	cfg.WithReadTimeout(time.Minute),
-	// 	cfg.WithDisableKeepAlive(false)))
+	h.AddProtocol("h2", factory.NewServerFactory(
+		cfg.WithReadTimeout(time.Minute),
+		cfg.WithDisableKeepAlive(false)))
 	// tlsCfg.NextProtos = append(tlsCfg.NextProtos, "h2")
 	// use pprof&&cors&&tracing&&sentinel
 	pprof.Register(h)
 	// h.Use(cors.New(corsCfg))
 	h.Use(hertztracing.ServerMiddleware(trcCfg))
 	h.Use(hertzSentinel.SentinelServerMiddleware(
+		hertzSentinel.WithServerResourceExtractor(func(c context.Context, ctx *app.RequestContext) string {
+			return consts.Tiktok
+		}),
 		// abort with status 429 by default
 		hertzSentinel.WithServerBlockFallback(func(c context.Context, ctx *app.RequestContext) {
 			ctx.JSON(http.StatusTooManyRequests, nil)
